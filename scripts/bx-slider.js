5e147d8c6bb47de7d1e77aac6d6a25cc9fa6f1cf
Veniamin Tsygulev <veniamin.c78@gmail.com>
2021-02-27 19:44:51 +0300

    Сделал swiper-slider, адаптировал три секции под 375рх

--- scripts/bx-slider.js
+++ scripts/bx-slider.js
@@ -1,17 +1,18 @@
-const slider = $('.products-slider__list').bxSlider({
-  pager: false,
-  controls: false
+$(document).ready(function(){
+  const slider = $('.products-slider__list').bxSlider({
+  //pager: false,
+  //controls: false
 });
 
 $('.products-slider__pointer_prev').click((e) => {
-  e.preventDefault();
-  
+  console.log(12321);
   slider.goToPrevSlide();
 });
 
 $('.products-slider__pointer_next').click((e) => {
   e.preventDefault();
-  
+  console.log(12321);
   slider.goToNextSlide();
 });
 
+});
--- scripts/products-slider.js
+++ scripts/products-slider.js
@@ -1,13 +1,33 @@
-$(document).ready(() => {
-  $('.products-slider__arrow_next').on('click', e => {
-    e.preventDefault();
-    $('.products-slider__list').css('left', '-100%');
-  });
-});
+// $(document).ready(() => {
+//   $('.products-slider__arrow_next').on('click', e => {
+//     e.preventDefault();
+//     $('.products-slider__list').css('left', '-100%');
+//   });
+// });
 
-$(document).ready(() => {
-  $('.products-slider__arrow_prev').on('click', e => {
-    e.preventDefault();
-    $('.products-slider__list').css('left', '0');
-  });
+// $(document).ready(() => {
+//   $('.products-slider__arrow_prev').on('click', e => {
+//     e.preventDefault();
+//     $('.products-slider__list').css('left', '0');
+//   });
+// });
+const swiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  //loop: true,
+
+  // If we need pagination
+  // pagination: {
+  //   el: '.swiper-pagination',
+  // },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.products-slider__arrow_next',
+    prevEl: '.products-slider__arrow_prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
 });
